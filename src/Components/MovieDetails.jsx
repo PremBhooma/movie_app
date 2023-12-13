aa0f2fa379cfb46e1c9cd74b610f2661eb6c8567
PremBhooma <bhoomasagar1213@gmail.com>
2023-12-13 18:53:07 +0530

    blur

--- src/Components/MovieDetails.jsx
+++ src/Components/MovieDetails.jsx
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import Header from "./Header";
+import { signOut } from "firebase/auth";
+import { auth } from "../Firebase/firebaseConfig";
 
 const MovieDetails = () => {
+  const user = JSON.parse(localStorage.getItem("user"));
+
   const { id } = useParams();
+  const navigate = useNavigate();
 
   const [movies, setMovies] = useState([]);
   const apiKey = "627d937a291b248bbaed756e1795900d";
@@ -22,28 +27,52 @@ const MovieDetails = () => {
     };
 
     fetchMovies();
-  }, [id]);
+  }, [id, apiKey]);
+
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      localStorage.removeItem("accessToken");
+      localStorage.removeItem("user");
+      navigate("/login");
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   const baseImageUrl = "https://image.tmdb.org/t/p/w500";
 
-  //   const releaseDate = new Date(movies.release_date);
-  //   const options = {
-  //     year: "numeric",
-  //     month: "long",
-  //     day: "numeric",
-  //   };
-  //   const formattedReleaseDate = new Intl.DateTimeFormat("en-US", options).format(
-  //     releaseDate
-  //   );
-
   return (
     <>
-      <Header />
-      <img
-        src={`${baseImageUrl}${movies.backdrop_path}`}
-        alt={movies.title}
-        className="card-img-top bootImg"
+      <Header
+        displayName={user.displayName}
+        image={user.photoURL}
+        handleLogout={handleLogout}
       />
+      <div
+        className="movie-details-container"
+        style={{
+          backgroundImage: `url(${baseImageUrl}${movies.backdrop_path})`,
+        }}
+      >
+        <div className="overlay"></div>
+        <div className="container ">
+          <div className="row">
+            <div className="col-md-4 overlay-content">
+              <img
+                src={`${baseImageUrl}${movies.poster_path}`}
+                alt={movies.title}
+                className="card-img-top bootImg"
+              />
+            </div>
+            <div className="col-md-8 overlay-content">
+              <h2>{movies.title}</h2>
+              <p>{movies.overview}</p>
+              <p>Release Date: {movies.release_date}</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
